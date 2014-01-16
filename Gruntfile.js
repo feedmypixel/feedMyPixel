5a450332a6412169c63bd692105467750e29bd9b
feedMyPixel <ben@feedmypixel.com>
2014-01-16 10:21:42 +0000

    revamped version of site, works cross browser/device down to ie6, minimal media queries, fonts and sprite fallback

---
+++ Gruntfile.js
@@ -0,0 +1,177 @@
+module.exports = function(grunt) {
+    'use strict';
+
+    var pkg = grunt.file.readJSON('package.json');
+
+    grunt.initConfig({
+
+        pkg: pkg,
+
+        dirs: {
+
+            public: {
+                root: 'public/'
+            },
+
+            dist: {
+                root: 'dist/',
+                assets: '<%= dirs.dist.root %>assets',
+                css: '<%= dirs.dist.root %>assets/css/',
+                js: '<%= dirs.dist.root %>assets/js/',
+                font: '<%= dirs.dist.root %>assets/font/',
+                img: '<%= dirs.dist.root %>assets/img/'
+            },
+
+            tmp: {
+                root: '.tmp'
+            }
+        },
+
+        clean: {
+
+            dist: [ '<%= dirs.dist.root %>' ],
+
+            assets: [
+                '<%= dirs.dist.css %>*.css',
+                '<%= dirs.dist.js %>*.js'
+            ],
+
+            tmp: [ '<%= dirs.tmp.root %>' ]
+        },
+
+        copy: {
+
+            options: {
+                encoding: 'utf8'
+            },
+
+            main: {
+
+                expand: true,
+                cwd: '<%= dirs.public.root %>',
+                src: '**',
+                dest: '<%= dirs.dist.root %>',
+                dot: true
+            }
+        },
+
+        uglify: {
+
+            options: {
+
+                report: 'gzip'
+            }
+        },
+
+        rev: {
+
+            options: {
+
+                encoding: 'utf8',
+                algorithm: 'md5',
+                length: 8
+            },
+
+            assets: {
+
+                files: [{
+
+                    src: [
+                        '<%= dirs.dist.img %>**/*.{png,jpg,gif,svg}',
+                        '<%= dirs.dist.font %>*.{eot,svg,ttf,woff}',
+                        '<%= dirs.dist.css %>*',
+                        '<%= dirs.dist.root %>*.{ico}'
+                    ]
+                }]
+            }
+        },
+
+        'useminPrepare': {
+
+            html: '<%= dirs.dist.root %>index.php'
+        },
+
+        usemin: {
+
+            html: {
+
+                src: [ '<%= dirs.dist.root %>index.php' ],
+
+                options: {
+
+                    assetsDirs: [
+
+                        '<%= dirs.dist.root %>',
+                        '<%= dirs.dist.assets %>'
+                    ]
+                }
+            },
+
+            css: {
+
+                src: '<%= dirs.dist.css %>*.css'
+            }
+        },
+
+        replace: {
+
+            application_environment_value: {
+
+                src: [ '<%= dirs.dist.root %>.htaccess' ],
+
+                overwrite: true,
+
+                replacements: [{
+
+                    from: /(development)/g,
+
+                    to: 'production'
+                }]
+            }
+        },
+
+        imagemin: {
+
+            dynamic: {
+
+                files: [{
+
+                    expand: true,
+
+                    cwd: '<%= dirs.dist.root %>',
+
+                    src: [ '**/*.{png,jpg,gif}' ],
+
+                    dest: '<%= dirs.dist.root %>'
+                }]
+            }
+        }
+    });
+
+    // tasks
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-rev');
+    grunt.loadNpmTasks('grunt-usemin');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-text-replace');
+    grunt.loadNpmTasks('grunt-contrib-imagemin');
+
+    // default task
+    grunt.registerTask('default', 'Create a dist build ready for production', [
+        'clean:dist',
+        'copy',
+        'useminPrepare',
+        'concat',
+        'clean:assets',
+        'cssmin',
+        'uglify',
+        'rev',
+        'usemin',
+        'clean:tmp',
+        'replace',
+        'imagemin'
+    ]);
+};
---
+++ public/index.php
@@ -0,0 +1,183 @@
+<!DOCTYPE html>
+<!--[if !IE]><!-->
+<!--
+  __                  _                   ___  _              _
+ / _|  ___   ___   __| |  /\/\   _   _   / _ \(_)__  __  ___ | |
+| |_  / _ \ / _ \ / _` | /    \ | | | | / /_)/| |\ \/ / / _ \| |
+|  _||  __/|  __/| (_| |/ /\/\ \| |_| |/ ___/ | | >  < |  __/| |
+|_|   \___| \___| \__,_|\/    \/ \__, |\/     |_|/_/\_\ \___||_| 2013
+                                 |___/
+!-->
+<!--<![endif]-->
+
+<!--[if lt IE 7]>  <html class="ie ie6 lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>     <html class="ie ie7 lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>     <html class="ie ie8 lt-ie9"> <![endif]-->
+<!--[if IE 9]>     <html class="ie ie9"> <![endif]-->
+<!--[if gt IE 9]>  <html> <![endif]-->
+<!--[if !IE]><!--> <html> <!--<![endif]-->
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport"                       content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
+    <meta name="HandheldFriendly"               content="true"/>
+    <meta name="apple-touch-fullscreen"         content="yes" />
+    <meta name="apple-mobile-web-app-capable"   content="yes">
+    <meta name="author"                         content="feedMyPixel&#46;com"/>
+    <meta name="description"                    content="feedMyPixel - script clever"/>
+    <meta name="keywords"                       content="feedMyPixel, ben chidgey, script clever"/>
+    <meta name="copyright"                      content="LazyGramophone&copy;"/>
+    <meta name="rating"                         content="general"/>
+    <meta http-equiv="imagetoolbar"             content="no"/>
+    <meta name="Rating"                         content="general"/>
+    <meta name="Distribution"                   content="Global"/>
+    <meta http-equiv="content-script-type"      content="text/javascript"/>
+
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+
+    <!-- build:css assets/css/fmp.css -->
+    <link rel="stylesheet" type="text/css" href="assets/css/reset.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/base.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/header.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/heading.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/layout.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/font.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/projects.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/type.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/footer.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/mediaQuery.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/ie.css">
+    <!-- endbuild -->
+
+    <title>feedMyPixel</title>
+    
+    <script type="text/javascript">
+
+        var _gaq = _gaq || [];
+        _gaq.push(['_setAccount', 'UA-3784467-17']);
+        _gaq.push(['_trackPageview']);
+
+        (function() {
+            var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+            ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+            var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+        })();
+
+    </script>
+</head>
+<body>
+    <div class="page-container">
+        <div class="header group">
+            <h1 class="outside-viewport-text">Ben Chidgey | feedMyPixel</h1>
+            <img class="logo" src="assets/img/feedmypixel.jpg" alt="feedMyPixel Ltd" />
+        </div>
+        <ul class="projects-container group">
+            <li class="project group">
+                <a href="http://time.lazygramophone.com" class="image-link font-13pt" target="_blank">
+                    <img class="image" src="assets/img/time.jpg" alt="Time."/>
+                </a>
+                <div class="description">
+                    <h2 class="font-18pt">Time.</h2>
+                    <p class="font-13pt">
+                        A mini site for the collaborative project Time, from the Independent Publisher &amp; Arts Collective Lazy Gramophone.
+                    </p>
+                    <a href="http://time.lazygramophone.com" class="project-link font-13pt" title="Time. - A collaborative project from Lazy Gramophone | Arts Collective & Independent Publisher" target="_blank">time.lazygramophone.com</a>
+                </div>
+                <ul class="tags font-11pt">
+                    <li class="tag">Vanilla Js</li>
+                    <li class="tag">Php</li>
+                    <li class="tag">Responsive</li>
+                    <li class="tag">Design</li>
+                    <li class="tag">UX/UI</li>
+                    <li class="tag">Grunt</li>
+                </ul>
+            </li>
+            <li class="project group">
+                <a href="http://jsfiddle.net/chidge/3ach8/" class="image-link" target="_blank">
+                    <img class="image" src="assets/img/grassyGnoll.jpg" alt="Space, the final grassy gnoll"/>
+                </a>
+                <div class="description">
+                    <h2 class="font-18pt">Grassy Gnoll</h2>
+                    <p class="font-13pt">
+                        A vanilla JS example of a star screensaver for the MooTools blog JavaScript Challenge #4 (Space, the final grassy gnoll).
+                    </p>
+                    <a href="http://jsfiddle.net/chidge/3ach8/" class="project-link font-13pt" title="Js fiddle for MooTools blog JavaScript Challenge #4" target="_blank">Js fiddle for Space, the final grassy gnoll</a>
+                    <a href="http://mootools.net/blog/2012/08/15/javascript-challenge-4/" class="project-link font-13pt" title="MooTools blog JavaScript Challenge #4 (Space, the final grassy gnoll)" target="_blank">MooTools blog JavaScript Challenge #4</a>
+                </div>
+                <ul class="tags font-11pt">
+                    <li class="tag">Vanilla Js</li>
+                </ul>
+            </li>
+            <li class="project group">
+                <a href="http://jsfiddle.net/chidge/ufrh6/" class="image-link" target="_blank">
+                    <img class="image" src="assets/img/whacAMoo.jpg" alt="Whac A Moo"/>
+                </a>
+                <div class="description">
+                    <h2 class="font-18pt">Whac A Moo</h2>
+                    <p class="font-13pt">
+                        A vanilla JS implementation of Whac A Mole for the MooTools blog JavaScript Challenge #2: (Whac A Moo).
+                    </p>
+                    <a href="http://jsfiddle.net/chidge/ufrh6/" class="project-link font-13pt" title="Js fiddle for MooTools blog JavaScript Challenge #2" target="_blank">Js fiddle for Whac A Moo</a>
+                        <a href="http://mootools.net/blog/2012/08/22/javascript-challenge-2-review/" class="project-link font-13pt" title="MooTools blog JavaScript Challenge #2 (Whac A Moo)" target="_blank">MooTools blog JavaScript Challenge #2</a>
+                </div>
+                <ul class="tags font-11pt">
+                    <li class="tag">Vanilla Js</li>
+                    <li class="tag">Design</li>
+                </ul>
+            </li>
+            <li class="project group">
+                <a href="http://www.lazygramophone.com" class="image-link" target="_blank">
+                    <img class="image" src="assets/img/lazyGramophone.jpg" alt="Lazy Gramophone"/>
+                </a>
+                <div class="description">
+                    <h2 class="font-18pt">Lazy Gramophone</h2>
+                    <p class="font-13pt">
+                        Lazy Gramophone is a content driven, community website for the arts collective and publisher.
+                        The website and Content Management System have been built from the ground up. Made in 2010.
+                    </p>
+                    <a href="http://www.lazygramophone.com" class="project-link font-13pt" title="Visit Lazy Gramophone" target="_blank">www.lazygramophone.com</a>
+                </div>
+                <ul class="tags font-11pt">
+                    <li class="tag">Vanilla Js</li>
+                    <li class="tag">Php</li>
+                    <li class="tag">MySQL</li>
+                    <li class="tag">Design</li>
+                    <li class="tag">UX/UI</li>
+                    <li class="tag">Hand built CMS</li>
+                </ul>
+            </li>
+        </ul>
+
+        <div class="footer group">
+            <ul class="social">
+                <li class="social-section">
+                    <a href="https://twitter.com/feedmypixel" target="_blank" class="icon twitter font-30pt"></a>
+                </li>
+                <li class="social-section">
+                    <a href="http://www.linkedin.com/in/benchidgey" target="_blank" class="icon linked-in font-30pt"></a>
+                </li>
+                <li class="social-section">
+                    <a href="https://github.com/feedmypixel" target="_blank" class="icon github font-30pt"></a>
+                </li>
+            </ul>
+
+            <a href="mailto:hello&#64;feedmypixel.com" class="font-13pt" title="contact feedMyPixel">hello&#64;feedMyPixel.com</a>
+
+            <p class="font-11pt">
+             <?php
+
+                ini_set('date.timezone', 'Europe/London');
+                $startYear   = (int)2009;
+                $currentYear = (int)date('Y');
+
+                $copyDate = $startYear === $currentYear ? $startYear : "{$startYear}-{$currentYear}";
+
+                echo "feedMyPixel.com {$copyDate} &copy;";
+
+                ?>
+            </p>
+            <p class="font-9pt">feedMyPixel Ltd is a company registered in England and Wales. Company number. 8198085</p>
+            <p class="font-9pt">VAT Registration Number 152 0607 46</p>
+        </div>
+    </body>
+</html>
